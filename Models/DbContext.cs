05cf1b9e2a1a7dc4d1cc1b01ba2a09cdbd055ed0
MullaAfiya <mullaafiya@gmail.com>
2024-08-22 20:53:00 +0530

    first commit

---
+++ Models/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CRUD_RegistrationAfiya_CO.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
